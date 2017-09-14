499885bef58d43386e5b6fa24d88346087d2d078
Jonas Johan solsvik <jonasjso@stud.ntnu.no>
2017-09-14 21:57:38 +0200

    small changes

--- main.go
+++ main.go
@@ -26,14 +26,6 @@ func ParseGitRepository(user string, repo string) (interface{}, error) {
 		Languages map[string]uint
 	}
 
-	type GitRepositoryOut struct {
-		Repository string   `json:"repository"` // e.g. Ordbase
-		Owner      string   `json:"owner"`      // e.g. FylkesmannenIKT
-		Committer  string   `json:"committer"`  // e.g. Arxcis
-		Commits    uint     `json:"commits"`    // e.g. 115
-		Languages  []string `json:"languages"`  // e.g. [shell, java, scala, ...]
-	}
-
 	githubRepo := &GitRepositoryIn{}
 	{
 		UnmarshalJSON := func(target interface{}, url string, wg *sync.WaitGroup, errorChannel chan error) {
@@ -76,6 +68,14 @@ func ParseGitRepository(user string, repo string) (interface{}, error) {
 		}
 	}
 
+	type GitRepositoryOut struct {
+		Repository string   `json:"repository"` // e.g. Ordbase
+		Owner      string   `json:"owner"`      // e.g. FylkesmannenIKT
+		Committer  string   `json:"committer"`  // e.g. Arxcis
+		Commits    uint     `json:"commits"`    // e.g. 115
+		Languages  []string `json:"languages"`  // e.g. [shell, java, scala, ...]
+	}
+
 	return GitRepositoryOut{
 		Repository: githubRepo.Name,
 		Owner:      githubRepo.Owner.Name,
@@ -92,27 +92,29 @@ func ParseGitRepository(user string, repo string) (interface{}, error) {
 	}, nil
 }
 
-func badRequestHandler(w http.ResponseWriter, r *http.Request) {
+// BadRequestHandler ...
+func BadRequestHandler(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 }
 
-func service(w http.ResponseWriter, r *http.Request) {
+// GitRepositoryHandler ...
+func GitRepositoryHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("content-type", "application/json")
 	vars := mux.Vars(r)
-	info, err := ParseGitRepository(vars["user"], vars["repo"])
+	parsedRepository, err := ParseGitRepository(vars["user"], vars["repo"])
 
 	if err != nil {
 		log.Println(err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
-	json.NewEncoder(w).Encode(info)
+	json.NewEncoder(w).Encode(parsedRepository)
 }
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/projectinfo/v1/github.com/", badRequestHandler)
-	router.HandleFunc("/projectinfo/v1/github.com/{user}", badRequestHandler)
-	router.HandleFunc("/projectinfo/v1/github.com/{user}/{repo}", service)
+	router.HandleFunc("/projectinfo/v1/github.com/", BadRequestHandler)
+	router.HandleFunc("/projectinfo/v1/github.com/{user}", BadRequestHandler)
+	router.HandleFunc("/projectinfo/v1/github.com/{user}/{repo}", GitRepositoryHandler)
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
 }
